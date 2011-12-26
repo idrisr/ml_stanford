6888bea1fee627bea305c68906e2425e479007ad
Idris Raja <idris.raja@gmail.com>
2011-12-26 11:46:53 -0800

    Implemented sigmoid function intended to take a m by 1 matrix and return its sigmoid value between 0 and 1. This sigmoid defined as a 1 / (1 + exp(-x))

--- cs229/02_log_reg/mlclass-ex2/log_reg.R
+++ cs229/02_log_reg/mlclass-ex2/log_reg.R
@@ -1,13 +1,20 @@
 require(ggplot2)
-data <- read.csv('ex2data1.txt', header=FALSE)
 
 plot_data <- function(data){
     g <- ggplot(data, aes(Exam1, Exam2, colour=Admitted)) + geom_point()
     g <- g + opts(title = "Admittance based on 2 Exams")
 }
 
+sigmoid <- function(X){
+    #Takes a m by 1 matrix and returns its sigmoid between 0 and 1
+    X <- 1 / (1+exp(-X))
+    return(X)
+}
+
+data <- read.csv('ex2data1.txt', header=FALSE)
 X <- data[, -length(data)]
 Y <- data[, length(data)]
 names(data) <- c('Exam1', 'Exam2', 'Admitted')
 data <- transform(data, Admitted = factor(Admitted))
 g <- plot_data(data)
+#ggsave(filename='initial_plot.jpeg', plot=g)
