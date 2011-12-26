4703e4b3ccaa18ccfaa8df018be4efa5a6fdafd6
Idris Raja <idris.raja@gmail.com>
2011-12-26 11:38:07 -0800

    Finished plot_data function to show color coded xy scatter plot of students admitted based on exam 1 and exam 2

--- cs229/02_log_reg/mlclass-ex2/log_reg.R
+++ cs229/02_log_reg/mlclass-ex2/log_reg.R
@@ -2,10 +2,12 @@ require(ggplot2)
 data <- read.csv('ex2data1.txt', header=FALSE)
 
 plot_data <- function(data){
-    ggplot(data,        
+    g <- ggplot(data, aes(Exam1, Exam2, colour=Admitted)) + geom_point()
+    g <- g + opts(title = "Admittance based on 2 Exams")
 }
 
 X <- data[, -length(data)]
 Y <- data[, length(data)]
-names(data) <- c('Exam 1', 'Exam 2', 'Admitted')
-
+names(data) <- c('Exam1', 'Exam2', 'Admitted')
+data <- transform(data, Admitted = factor(Admitted))
+g <- plot_data(data)
