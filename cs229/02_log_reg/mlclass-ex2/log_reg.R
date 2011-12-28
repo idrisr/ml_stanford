7c59cd2c1799528ddef55c05c6705a7f4b0dcc27
Idris Raja <idris.raja@gmail.com>
2011-12-27 17:56:42 -0800

    see notes in plot_decision_boundary for solution

--- cs229/02_log_reg/mlclass-ex2/log_reg.R
+++ cs229/02_log_reg/mlclass-ex2/log_reg.R
@@ -73,6 +73,15 @@ decision_boundary <- function(x, theta){
 }
 
 plot_decision_boundary <- function(data, theta){
+    #Do this
+    #g <- ggplot(data, aes(Exam1, Exam2, pred, Admitted))
+
+    ## add color
+    #g <- g + geom_point(aes(x=Exam1, y = Exam2))
+    #g <- g + geom_line(aes(x=Exam1, y = pred))
+
+
+        
     g <- plot_data(data)
     g <- g + geom_point(aes("Exam1", "pred"))
     return(g)
