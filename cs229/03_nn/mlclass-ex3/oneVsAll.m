ab6a7e67e4c505954b7af8e7adcdcbb890ded805
Idris Raja <idris.raja@gmail.com>
2012-01-02 13:52:51 -0800

    transposing theta before setting to a row of all_theta, and decreasing iterations from 50 to 5 to see if code runs

--- cs229/03_nn/mlclass-ex3/oneVsAll.m
+++ cs229/03_nn/mlclass-ex3/oneVsAll.m
@@ -49,7 +49,7 @@ X = [ones(m, 1) X];
 %                 initial_theta, options);
 %
 
-options = optimset('GradObj', 'on', 'MaxIter', 50);
+options = optimset('GradObj', 'on', 'MaxIter', 5);
 % Set Initial theta
 initial_theta = zeros(n + 1, 1);
 
@@ -57,7 +57,7 @@ initial_theta = zeros(n + 1, 1);
 for c=1:num_labels
      [theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), ...
              initial_theta, options);
-     all_theta(c) = theta;
+     all_theta(c) = theta';
 end
 
 % =========================================================================
