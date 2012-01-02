4964f59811f8803675d13bc9a57802cbf2eb1006
Idris Raja <idris.raja@gmail.com>
2012-01-02 12:42:20 -0800

    Syntax change to keep under 80 chars per line

--- cs229/03_nn/mlclass-ex3/displayData.m
+++ cs229/03_nn/mlclass-ex3/displayData.m
@@ -38,9 +38,10 @@ for j = 1:display_rows
 		
 		% Get the max value of the patch
 		max_val = max(abs(X(curr_ex, :)));
-		display_array(pad + (j - 1) * (example_height + pad) + (1:example_height), ...
-		              pad + (i - 1) * (example_width + pad) + (1:example_width)) = ...
-						reshape(X(curr_ex, :), example_height, example_width) / max_val;
+		display_array(
+            pad + (j - 1) * (example_height + pad) + (1:example_height), ...
+		    pad + (i - 1) * (example_width + pad) + (1:example_width)) = ...
+			reshape(X(curr_ex, :), example_height, example_width) / max_val;
 		curr_ex = curr_ex + 1;
 	end
 	if curr_ex > m, 
