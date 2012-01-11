ae0ffa2a45ca7fec7a3fd8fb5a63a9342eeb5408
Idris Raja <idris.raja@gmail.com>
2012-01-11 13:29:31 -0800

    A sweet little vectorized implementation

--- cs229/08_pca/mlclass-ex7/computeCentroids.m
+++ cs229/08_pca/mlclass-ex7/computeCentroids.m
@@ -26,12 +26,9 @@ centroids = zeros(K, n);
 % Note: You can use a for-loop over the centroids to compute this.
 %
 
-
-
-
-
-
-
+for k=1:K 
+    centroids(k,:) = mean(X(idx==k,:))
+end
 
 % =============================================================
 
